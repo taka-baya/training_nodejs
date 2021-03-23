eb13839ee7f95528685a6d36e8427377cd1d745b
taka.baya <kamiyabi12@gmail.com>
2021-03-23 14:42:04 +0900

    Learning Eventloop

--- first_project/index.js
+++ first_project/index.js
@@ -1,19 +1,24 @@
 
+// Timers
 // SetTimeout の遅延実行時間は100msec
 // SetTimeout の中身の処理は遅延実行される
 // JavaScriptはシングルスレッドなので、3秒間のループ処理後にSetTimeoutの中身が実行される
+// Timers
 setTimeout(()=>{
     console.log("setTimeout()");
-}, 100)
+}, 1)
 
-console.log("global");
+//check
+setImmediate(()=>{
+    console.log("setImmediate");
+})
 
-// 3秒間whileループ処理
-var end = (new Date()).getTime() + 3000;
-while ((new Date()).getTime() < end){ }
+// キュー(nextTickQueue)の実行
+process.nextTick(()=>{
+    console.log("nextTic");
+})
 
-//現在のディレクトリを取得
-console.log(__dirname);
-
-//現在実行中のファイルパスを取得
-console.log(__filename);
+// キュー(MicroTaskQueue)の実行
+Promise.resolve().then(() => {
+    console.log("Promise.resolve().then()");
+})
